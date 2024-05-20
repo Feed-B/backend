0946e606deadd1a3a8aa86b65efbbb6cc310c081
DK <laancer4@gmail.com>
2024-05-20 21:54:03 +0900

    feat : Security 설정 (수정필요)

--- src/main/java/com/example/team_12_be/member/domain/MemberRepository.java
+++ src/main/java/com/example/team_12_be/member/domain/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.example.team_12_be.member.domain;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
 }
