d3e5e0c660f2075a5d82d06d1c519cb3d4c8e756
DK <laancer4@gmail.com>
2024-05-20 21:00:24 +0900

    init

---
+++ src/main/java/com/example/team_12_be/Team12BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team_12_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team12BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team12BeApplication.class, args);
+    }
+
+}
