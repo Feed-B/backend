3fab7485cfc5e0932c84c7739145ffadeb1f89be
DK <laancer4@gmail.com>
2024-05-28 23:16:55 +0900

    refactor : @SQLDelete 이용한 소프트 딜리트

---
+++ src/main/java/com/example/team_12_be/post/application/PostService.java
@@ -0,0 +1,8 @@
+package com.example.team_12_be.post.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+}
---
+++ src/main/java/com/example/team_12_be/post/domain/ProjectLike.java
@@ -0,0 +1,36 @@
+package com.example.team_12_be.post.domain;
+
+import com.example.team_12_be.base.TimeStamp;
+import com.example.team_12_be.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ProjectLike extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // TODO : 인덱스? 아니면 추가 테이블?
+
+}
