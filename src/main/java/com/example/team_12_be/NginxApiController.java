61f1f50443905b09903c8447f98ed5e72577c857
gkstjr <gkstjr8332@naver.com>
2024-06-01 22:08:42 +0900

    feat : nginx 8081 / 8082 포드 설정

---
+++ src/main/java/com/example/team_12_be/NginxApiController.java
@@ -0,0 +1,29 @@
+package com.example.team_12_be;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+public class NginxApiController {
+
+    private final Environment env;
+
+    //무중단 배포 메인 페이지 테스트용(임시)
+    @RequestMapping("/")
+    public String hello() {
+        return "hello4";
+    }
+
+    @GetMapping("/nginx/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
--- src/main/java/com/example/team_12_be/Team12BeApplication.java
+++ src/main/java/com/example/team_12_be/Team12BeApplication.java
@@ -2,12 +2,18 @@ package com.example.team_12_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Team12BeApplication {
-
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:nginx-set.yml";
     public static void main(String[] args) {
-        SpringApplication.run(Team12BeApplication.class, args);
+
+        new SpringApplicationBuilder(Team12BeApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
