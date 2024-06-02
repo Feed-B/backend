7d70727fb880373a0d49cb9b14044bd6422b2a94
DK <laancer4@gmail.com>
2024-06-02 14:27:50 +0900

    fix : 쓸모없는 VO 제거

--- src/main/java/com/example/team_12_be/member/domain/Member.java
+++ src/main/java/com/example/team_12_be/member/domain/Member.java
@@ -1,11 +1,7 @@
 package com.example.team_12_be.member.domain;
 
 import com.example.team_12_be.base.TimeStamp;
-import com.example.team_12_be.member.domain.vo.AboutMe;
-import com.example.team_12_be.member.domain.vo.MemberEmail;
-import com.example.team_12_be.member.domain.vo.NickName;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,7 +17,7 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor
 @Getter
-@SQLDelete(sql = "UPDATE MEMBER SET IS_DELETED = true WHERE IS_DELETED = FALSE AND id= ?")
+@SQLDelete(sql = "UPDATE MEMBER SET IS_DELETED = true WHERE IS_DELETED = FALSE AND id = ?")
 public class Member extends TimeStamp {
 
     @Id
@@ -30,22 +26,19 @@ public class Member extends TimeStamp {
 
     private Boolean isDeleted;
 
-    @Embedded
-    private MemberEmail email;
+    private String email;
 
-    @Embedded
-    private NickName nickName;
+    private String nickName;
 
-    @Embedded
-    private AboutMe aboutMe;
+    private String aboutMe;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<MemberTechStack> memberTechStacks = new LinkedHashSet<>();
 
-    public Member(MemberEmail email, NickName nickName, AboutMe aboutMe) {
+    public Member(String email, String nickName, String aboutMe) {
+        this.isDeleted = Boolean.FALSE;
         this.email = email;
         this.nickName = nickName;
         this.aboutMe = aboutMe;
-        this.isDeleted = Boolean.FALSE;
     }
 }
