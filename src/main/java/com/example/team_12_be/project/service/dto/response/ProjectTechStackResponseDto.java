c2374d8136415545db08d37b2a0081070e3f022e
DK <laancer4@gmail.com>
2024-06-09 22:01:44 +0900

    fix : 변경

--- src/main/java/com/example/team_12_be/project/service/dto/response/ProjectTechStackResponseDto.java
+++ src/main/java/com/example/team_12_be/project/service/dto/response/ProjectTechStackResponseDto.java
@@ -1,12 +1,12 @@
 package com.example.team_12_be.project.service.dto.response;
 
-import com.example.team_12_be.member.domain.vo.Job;
 import com.example.team_12_be.project.domain.ProjectTechStack;
 
-public record ProjectTechStackResponseDto (Job job) {
+public record ProjectTechStackResponseDto (String techStack) {
 
     public static ProjectTechStackResponseDto of(ProjectTechStack projectTechStack) {
         return new ProjectTechStackResponseDto(projectTechStack.getTechStack());
     }
+
 
 }
