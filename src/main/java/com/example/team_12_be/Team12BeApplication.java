b8954f7c60a74a0a5fd5b9a2618393c8d1a22132
DK <laancer4@gmail.com>
2024-06-17 22:28:43 +0900

    test : CI

--- src/main/java/com/example/team_12_be/Team12BeApplication.java
+++ src/main/java/com/example/team_12_be/Team12BeApplication.java
@@ -7,20 +7,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Team12BeApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:nginx-set.yml";
-    public static void main(String[] args) {
-        //보안을 위한 .env 파일 사용
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
-        System.setProperty("JWT_SECRET" , dotenv.get("JWT_SECRET"));
+//    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//            + "classpath:application.yml,"
+//            + "classpath:nginx-set.yml";
+//    public static void main(String[] args) {
+//        //보안을 위한 .env 파일 사용
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
+//        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+//        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
+//        System.setProperty("JWT_SECRET" , dotenv.get("JWT_SECRET"));
+//
+//        new SpringApplicationBuilder(Team12BeApplication.class)
+//                .properties(APPLICATION_LOCATIONS)
+//                .run(args);
+//    }
 
-        new SpringApplicationBuilder(Team12BeApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(Team12BeApplication.class, args);
     }
 
 }
