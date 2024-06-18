328206064a33ab3a0b0080836118ed652ea11853
DK <laancer4@gmail.com>
2024-06-18 19:38:21 +0900

    feat : Swagger 용 SecurityRequirement 추가

--- src/main/java/com/example/team_12_be/project/comment/presentation/ProjectCommentQueryController.java
+++ src/main/java/com/example/team_12_be/project/comment/presentation/ProjectCommentQueryController.java
@@ -4,6 +4,7 @@ import com.example.team_12_be.global.page.CustomPageResponse;
 import com.example.team_12_be.project.comment.service.dto.ProjectCommentResponseDto;
 import com.example.team_12_be.project.comment.service.ProjectCommentQueryService;
 import com.example.team_12_be.project.comment.service.dto.ReplyCommentResponseDto;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "프로젝트 댓글 GET(조회) 컨트롤러")
 @RestController
 @RequiredArgsConstructor
+@SecurityRequirement(name = "Bearer Authentication")
 public class ProjectCommentQueryController {
 
     private final ProjectCommentQueryService projectCommentQueryService;
