8d9d4ff15edb3e6683b13ca217c3a929d69e8771
DK <laancer4@gmail.com>
2024-06-18 22:28:45 +0900

    fix : Job 명세대로 변경

--- src/main/java/com/example/team_12_be/member/domain/vo/Job.java
+++ src/main/java/com/example/team_12_be/member/domain/vo/Job.java
@@ -1,10 +1,14 @@
 package com.example.team_12_be.member.domain.vo;
 
 public enum Job {
+    ALL,
     FRONTEND,
     BACKEND,
     DESIGNER,
     IOS,
     ANDROID,
-    DEVOPS
+    DEVOPS,
+    PLANNER,
+    FULLSTACK,
+    ETC,
 }
