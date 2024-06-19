789c1c323d6934c47554ac92d720598bc2464ed3
DK <laancer4@gmail.com>
2024-06-19 17:49:21 +0900

    fix : response 에 누락된 idx 필드 추가

--- src/main/java/com/example/team_12_be/project/service/dto/response/ProjectImageResponseDto.java
+++ src/main/java/com/example/team_12_be/project/service/dto/response/ProjectImageResponseDto.java
@@ -4,12 +4,14 @@ import com.example.team_12_be.project.domain.ProjectImage;
 
 public record ProjectImageResponseDto(
         Long id,
-        String url
+        String url,
+        int idx
 ) {
     public static ProjectImageResponseDto of(ProjectImage projectImage){
         return new ProjectImageResponseDto(
                 projectImage.getId(),
-                projectImage.getUrl()
+                projectImage.getUrl(),
+                projectImage.getIndex()
         );
     }
 }
