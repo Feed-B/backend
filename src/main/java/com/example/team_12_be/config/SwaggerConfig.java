254dcdaa25ef7331cc047b45197aaa9170ea914f
gkstjr <gkstjr8332@naver.com>
2024-06-22 21:30:55 +0900

    fix : swaggerConfig(우선 운영 도메인으로 하드 코딩 배포 후 동적 설정 구현)

--- src/main/java/com/example/team_12_be/config/SwaggerConfig.java
+++ src/main/java/com/example/team_12_be/config/SwaggerConfig.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(version = "1.0"))
+@OpenAPIDefinition(info = @Info(version = "1.0") , servers = {@Server(url = "https://feedb.shop", description = "Default Server URL")})
+
 @SecurityScheme(
         name = "Bearer Authentication",
         type = SecuritySchemeType.HTTP,
@@ -15,4 +17,6 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class SwaggerConfig {
+
+
 }
